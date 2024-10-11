eb07545680b3029837fdfc55abd10f21fe474d17
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-11 17:58:33 +0545

    catch finally

--- promises.js
+++ promises.js
@@ -1,28 +1,50 @@
- const promiseOne = new Promise(function(resolve , reject){
-setTimeout(function(){
-    console.log('Async task is complete');
-    resolve()
-},1000)
- })
+const promiseOne = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
+});
 
- promiseOne.then(function(){
-    console.log("Promise consumed");
- })
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
 
- new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    },1000);
- }).then(function(){
-    console.log("Async 2 resolved");
- })
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
 
- const promise3 = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username:"Biku",email:"bikash.acvharya614@gmail.com"})
-    },1000)
- })
- promise3.then(function(user){
-console.log(user)
- })
+const promise3 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Biku", email: "bikash.acvharya614@gmail.com" });
+  }, 1000);
+});
+promise3.then(function (user) {
+  console.log(user);
+});
+const promise4 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "Bikash", password: "123" });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  }, 1000);
+});
+promise4
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => console.log("The promise is either resolved or rejected"))
